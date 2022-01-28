ff6253d40eb4b9f0ad41ca2500699591f18fa219
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-29 02:18:44 +0600

    object file added

---
+++ object.js
@@ -0,0 +1,27 @@
+// Object 1
+var student = {
+    id: 120,
+    name: 'mofazzal',
+    mark: 75,
+    phone: '01755236630'
+
+};
+// object 2
+var mobile = {
+    color: 'black',
+    price: 230000,
+    screenSize: 5,
+    Storage: '128gb',
+}
+// object-3
+var book = {
+    title : 'start with why',
+    author : 'simon sinek',
+    price: 299,
+    page: 244,
+    publisher: 'penguin'
+};
+console.log(student);
+console.log(mobile);
+console.log(book);
+
